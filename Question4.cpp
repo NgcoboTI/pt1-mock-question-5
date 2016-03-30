6f2d2e538004a7cc2771a37dc46f83431c705e35
Thalnte ngcobo <mrti.ngcobo@outlook.com>
2016-03-30 13:49:46 +0200

    commit

--- Question4.cpp
+++ Question4.cpp
@@ -11,3 +11,56 @@
 // 4.2 From class "Animal", derive a concrete class "Bird" and "Dog".  Print appropriate message for
 //     each method.
 // 4.3 Write a driver program which creates an object of each type and demonstrates POLYMORPHISM. 
+#include <iostream>
+
+using namespace std;
+class Animal{
+public :
+virtual void name(){
+		cout << "Animal" << endl;
+	}
+virtual void does(){
+		cout << "Nothing" << endl;
+	}
+};
+
+class Bird : public Animal{
+public:
+	void name(){
+		cout << "Bird" << endl;
+	}
+	void does(){
+		cout << "Fly" << endl;
+	}
+};
+
+class Dog : public Animal{
+public:
+	void name(){
+		cout << "Dog" << endl;
+	}
+	void does(){
+		cout << "Bark" << endl;
+	}
+};
+
+
+int main(){
+	Animal *ptr;
+
+	ptr = &Animal();
+
+	ptr->name();
+	ptr->does();
+
+	ptr = &Bird();
+	ptr->name();
+	ptr->does();
+
+	ptr = &Dog();
+	ptr->name();
+	ptr->does();
+
+      
+
+}
